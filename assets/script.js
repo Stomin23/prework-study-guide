0fb46d58287119586f9a32bbe45cb66569eb36f2
Stomin23 <152355060+Stomin23@users.noreply.github.com>
2023-12-05 22:55:53 -0500

    <I like coding>

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -17,3 +18,4 @@ var shapes = ["triangle", "square", "pentagon", "circle"];
 for(var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
    }
+   */
